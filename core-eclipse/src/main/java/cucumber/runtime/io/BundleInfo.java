ea6af8495ff806170e91c24b317a969b7e666dc7
HeikoLuch <kopp66@gmx.net>
2019-01-02 06:22:38 +0100

    new fragment for core

---
+++ core-eclipse/src/main/java/cucumber/runtime/io/BundleInfo.java
@@ -0,0 +1,85 @@
+package cucumber.runtime.io;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+import org.osgi.framework.Bundle;
+
+public class BundleInfo {
+
+	private void printBundleInfo1(Bundle b) throws IOException {
+
+		System.out.println("\n==== INFO: Current Bundle Symbolic Name '" + b.getSymbolicName() + " ====\n");
+
+		// Listet die eigenen Classen auf. Ausgabe bspw.
+		// findEntries('/', '*.class', true):
+		// bundleentry://30.fwk1302227152/cucumber/api/event/TestCaseFinished.class
+		//
+		System.out.println("  ---- Find entries ----");
+		Enumeration<URL> urls = b.findEntries("/", "*.class", true);
+		if (urls != null) {
+			while (urls.hasMoreElements()) {
+				URL url = urls.nextElement();
+				System.out.println("  findEntries('/', '*.class', true): " + url.toExternalForm());
+
+				// Versuche, den Filepath zu ermitteln
+				try {
+					String filepath = Helpers.filePath(url);
+					System.out.println("     filepath = '" + filepath + "'");
+
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+		}
+		// printResources1(b);
+		System.out.println("======== END: '" + b.getSymbolicName() + " ==========\n");
+
+	}
+
+	private void printResources(Bundle b) throws IOException {
+
+		System.out.println("\n  ---- Get Resources ----");
+
+		String name = "";
+		Enumeration<URL> urls = b.getResources("");
+		if (urls != null) {
+			while (urls.hasMoreElements()) {
+				URL url = urls.nextElement();
+				System.out.println("  getResources('" + name + "'): " + url.toExternalForm());
+			}
+		} else
+			System.out.println("  getResources('" + name + "'): " + "returned NOTHING");
+
+	}
+
+	private void printBundleState(Bundle b) {
+		// Determine bundle state as string
+		int state = b.getState();
+		String strState = "";
+
+		if ((state & Bundle.INSTALLED) > 0)
+			strState += "INSTALLED ";
+		if ((state & Bundle.ACTIVE) > 0)
+			strState += "ACTIVE ";
+		if ((state & Bundle.RESOLVED) > 0)
+			strState += "RESOLVED ";
+		if ((state & Bundle.UNINSTALLED) > 0)
+			strState += "UNINSTALLED ";
+		if ((state & Bundle.STARTING) > 0)
+			strState += "STARTING ";
+		if ((state & Bundle.STOPPING) > 0)
+			strState += "STOPPING ";
+		if ((state & Bundle.START_TRANSIENT) > 0)
+			strState += "START_TRANSIENT ";
+		if ((state & Bundle.START_ACTIVATION_POLICY) > 0)
+			strState += "START_ACTIVATION_POLICY ";
+		if ((state & Bundle.STOP_TRANSIENT) > 0)
+			strState += "STOP_TRANSIENT ";
+		if (strState.length() == 0)
+			strState = "Starteing, Stopping , ???";
+
+		System.out.println("  Bundle State '" + b.getSymbolicName() + "': " + b.getState() + " [" + strState + "] ");
+	}
+}
--- core/src/main/java/cucumber/runtime/io/ClasspathResourceIterable.java
+++ core/src/main/java/cucumber/runtime/io/ClasspathResourceIterable.java
@@ -8,56 +8,35 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.util.Enumeration;
 import java.util.Iterator;
-//TODO: Move to fragment
+
 public class ClasspathResourceIterable implements Iterable<Resource> {
-	
-	//Gibt entweder eine andere ResourceIteratorFactory zurück (java.lang.ServiceLoader) oder die Fallback-Variante.
-	private final ResourceIteratorFactory resourceIteratorFactory = new DelegatingResourceIteratorFactory(
-			new ZipThenFileResourceIteratorFactory()); //fallback
+    private final ResourceIteratorFactory resourceIteratorFactory =
+            new DelegatingResourceIteratorFactory(new ZipThenFileResourceIteratorFactory());
 
-//	// ======== Determine implementation for Bundle Fragment =======
-//	private static final BundleResourceLoader IMPL;
-//	static {
-//		// try to load class FactoryDateTimeImpl from fragment
-//		IMPL = (BundleResourceLoader) ImplementationLoader.newInstance(BundleResourceLoader.class);
-//	}
-//	// ======== END: Determine implementation for Bundle Fragment =======
+    private final ClassLoader classLoader;
+    private final String path;
+    private final String suffix;
 
-	private final ClassLoader classLoader;
-	private final String path;
-	private final String suffix;
+    public ClasspathResourceIterable(ClassLoader classLoader, String path, String suffix) {
+        this.classLoader = classLoader;
+        this.path = path;
+        this.suffix = suffix;
+    }
 
-	public ClasspathResourceIterable(ClassLoader classLoader, String path, String suffix) {
-		this.classLoader = classLoader; //ignore in OSGI
-		this.path = path;
-		this.suffix = suffix;
-	}
-
-	@Override
-	public Iterator<Resource> iterator() {
-		try {
-			FlatteningIterator<Resource> iterator = new FlatteningIterator<Resource>();
-			//Original:
-			//Enumeration<URL> resources = classLoader.getResources(path);
-			//New:
-			Enumeration<URL> resources = new BundleResourceAccessor().getResources(path, suffix); 
-
-
-			while (resources.hasMoreElements()) {
-				URL url = resources.nextElement();
-//System.out.println("Investigating URL: " + url.toString());				
-				
-				//url: 		bundleentry://5.fwk1645547422/bin/com/avenqo/cucumber/example/appl/swtbot/runner/RunCukesTest.class
-				//path: 	com/avenqo/cucumber/example/appl/swtbot/runner
-				//suffix: 	.class
-				Iterator<Resource> resourceIterator = resourceIteratorFactory.createIterator(url, path, suffix);
-				iterator.push(resourceIterator);
-			}
-
-			return iterator;
-		} catch (Exception e) { 
-			throw new CucumberException(e);
-		}
-	}
+    @Override
+    public Iterator<Resource> iterator() {
+        try {
+            FlatteningIterator<Resource> iterator = new FlatteningIterator<Resource>();
+            Enumeration<URL> resources = classLoader.getResources(path);
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                Iterator<Resource> resourceIterator = resourceIteratorFactory.createIterator(url, path, suffix);
+                iterator.push(resourceIterator);
+            }
+            return iterator;
+        } catch (IOException e) {
+            throw new CucumberException(e);
+        }
+    }
 
 }
